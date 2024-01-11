2edcf97fc913efe29d0289a8018a42b56745460c
Dan Peroff <peroff@protonmail.com>
2024-01-11 18:28:29 +0300

    Write getGameAnswer() func

---
+++ game.go
@@ -0,0 +1,40 @@
+package main
+
+// Предположительная функция генерации ответа игры (информации об угаданных
+// буквах). С ее учетом строится алгоритм фильтрации слов.
+func getGameAnswer(secret, try string) string {
+	// результат работы
+	answerChars := make([]rune, wordLen)
+
+	// массивы символов загаданного слова и предположения
+	secretChars := []rune(secret)
+	tryChars := []rune(try)
+	if len(secretChars) != wordLen || len(tryChars) != wordLen {
+		panic("bad word length")
+	}
+
+	// число вхождений каждой буквы в загаданное слово
+	secretCharCount := make(map[rune]int)
+	for _, sc := range secretChars {
+		secretCharCount[sc]++
+	}
+
+	// сколько раз каждую букву уже раскрыли текущим предположением
+	openCharCount := make(map[rune]int)
+
+	for i, tc := range tryChars {
+		if tc == secretChars[i] {
+			answerChars[i] = '+'
+			openCharCount[tc]++
+		} else {
+			if openCharCount[tc] < secretCharCount[tc] {
+				answerChars[i] = '.'
+				openCharCount[tc]++
+			} else {
+				answerChars[i] = '-'
+			}
+		}
+	}
+
+	return string(answerChars)
+}
